1ca32c2903a2280b9d784e878876ab96bff6e4e3
ChenSir <1474591391@qq.com>
2022-02-21 15:47:13 +0800

    完成了tabbar基础配置

---
+++ unpackage/dist/dev/mp-weixin/pages/Cart/Cart.js
@@ -0,0 +1,31 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/Cart/Cart"],{
+
+/***/ 34:
+/*!************************************************************************************!*\
+  !*** C:/Users/ChenSir/Desktop/ShoppingMall/main.js?{"page":"pages%2FCart%2FCart"} ***!
+  \************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 3));
+var _Cart = _interopRequireDefault(__webpack_require__(/*! ./pages/Cart/Cart.vue */ 35));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_Cart.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 35:
+/*!*****************************************************************!*\
+  !*** C:/Users/ChenSir/Desktop/ShoppingMall/pages/Cart/Cart.vue ***!
+  \*****************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\ChenSir\\Desktop\\ShoppingMall\\pages\\Cart\\Cart.vue'");
+
+/***/ })
+
+},[[34,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/Cart/Cart.js.map
---
+++ unpackage/dist/dev/mp-weixin/pages/My/My.js
@@ -0,0 +1,31 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/My/My"],{
+
+/***/ 40:
+/*!********************************************************************************!*\
+  !*** C:/Users/ChenSir/Desktop/ShoppingMall/main.js?{"page":"pages%2FMy%2FMy"} ***!
+  \********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 3));
+var _My = _interopRequireDefault(__webpack_require__(/*! ./pages/My/My.vue */ 41));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_My.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 41:
+/*!*************************************************************!*\
+  !*** C:/Users/ChenSir/Desktop/ShoppingMall/pages/My/My.vue ***!
+  \*************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\ChenSir\\Desktop\\ShoppingMall\\pages\\My\\My.vue'");
+
+/***/ })
+
+},[[40,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/My/My.js.map
