d87ac817de9d09c061333406b1425b2d3a405338
ChenSir <1474591391@qq.com>
2022-03-03 20:35:13 +0800

    自定义goods组件中添加单选组件和数字输入框组件

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -2,13 +2,26 @@
   <view class="goods-item">
     <!-- 商品左侧图片区域 -->
     <view class="goods-item-left">
+      <radio
+        :checked="item.goods_state"
+        @click="radioClickHandler"
+        color="#c00000"
+        v-if="showRadio"
+      />
       <image :src="item.goods_small_logo || defaultImg"></image>
     </view>
     <!-- 商品右侧信息区域 -->
     <view class="goods-item-right">
       <view class="goods-name">{{ item.goods_name }}</view>
-      <view class="goods-info-bpx">
+      <view class="goods-info-box">
         <view class="goods-price">￥{{ item.goods_price | tofixed }}</view>
+        <!-- 商品数量 -->
+        <uni-number-box
+          :min="1"
+          :value="goods.goods_count"
+          @change="numChangeHandler"
+          v-if="showNum"
+        ></uni-number-box>
       </view>
     </view>
   </view>
@@ -17,9 +30,20 @@
 <script>
 export default {
   props: {
+    //父级传递的goods对象
     item: {
       type: Object,
       default: {},
+    },
+    //是否显示单选框
+    showRadio: {
+      type: Boolean,
+      default: false,
+    },
+    //是否显示右侧number数字组件
+    showNum: {
+      type: Boolean,
+      default: false,
     },
   },
   data: () => ({
@@ -32,7 +56,23 @@ export default {
     tofixed: (num) => Number(num).toFixed(2),
   },
   computed: {},
-  methods: {},
+  methods: {
+    radioClickHandler() {
+      //触发父级的自定义事件，同时传递商品id与最新勾选状态
+      this.$emit("radio-change", {
+        goods_id: this.item.goods_id,
+        goods_state: !this.item.goods_state,
+      });
+    },
+    //bumberBox组件的change事件处理
+    numChangeHandler(val) {
+      //拿到最新的数量传递到父组件
+      this.$emit("num-change", {
+        goods_id: this.item.goods_id,
+        goods_count: +val,
+      });
+    },
+  },
   watch: {},
 
   // 组件周期函数--监听组件挂载完毕
@@ -57,6 +97,9 @@ export default {
   border-bottom: 1px solid #f0f0f0;
   .goods-item-left {
     margin-left: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     image {
       width: 100px;
       height: 100px;
@@ -65,6 +108,7 @@ export default {
   .goods-item-right {
     margin-left: 10px;
     display: flex;
+    flex: 1;
     flex-direction: column;
     justify-content: space-between;
     .goods-name {
@@ -73,6 +117,11 @@ export default {
     .goods-price {
       font-size: 16px;
       color: #c00000;
+    }
+    .goods-info-box {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     }
   }
 }
