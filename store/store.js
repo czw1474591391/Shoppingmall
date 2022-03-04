115f8ba28d9804edde665f42a600bece3d407e29
ChenSir <1474591391@qq.com>
2022-03-04 14:24:36 +0800

    vuex增加收货地址模块

--- store/store.js
+++ store/store.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import moduleCart from "./cart";
+import modeleUser from "./user";
 Vue.use(Vuex);
 
 //创建store实例对象
@@ -9,6 +10,7 @@ const store = new Vuex.Store({
   modules: {
     //挂载购物车的vuex模块，购物车中的cart数组的访问路径是m_cart/cart
     m_cart: moduleCart,
+    m_user: modeleUser,
   },
 });
 
---
+++ store/user.js
@@ -0,0 +1,32 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    address: JSON.parse(uni.getStorageSync("address") || "{}"),
+  }),
+  mutations: {
+    //更新收货地址
+    updataAddress(state, address) {
+      state.address = address;
+      //调用存储方法
+      this.commit("m_user/saveAddressToStorage");
+    },
+    //将地址持久化存储到本地
+    saveAddressToStorage() {
+      uni.setStorageSync("address", JSON.stringify(state.address));
+    },
+  },
+  // 数据包装器
+  getters: {
+    // 收货详细地址的计算属性
+    addstr() {
+      if (!this.address.provinceName) return "";
+      // 拼接 省，市，区，详细地址 的字符串并返回给用户
+      return (
+        this.address.provinceName +
+        this.address.cityName +
+        this.address.countyName +
+        this.address.detailInfo
+      );
+    },
+  },
+};
