8b3958a4ad32d7be8a5b83c05fef5131d41d8ea3
ChenSir <1474591391@qq.com>
2022-03-07 22:15:58 +0800

    user模块中增加更新用户信息以及token的方法

--- store/user.js
+++ store/user.js
@@ -1,7 +1,12 @@
 export default {
+  //开启命名空间
   namespaced: true,
   state: () => ({
     address: JSON.parse(uni.getStorageSync("address") || "{}"),
+    //登录之后的token字符串
+    token: uni.getStorageSync("token") || "",
+    //用户基本信息
+    userinfo: JSON.parse(uni.getStorageSync("userinfo") || "{}"),
   }),
   mutations: {
     //更新收货地址
@@ -13,6 +18,24 @@ export default {
     //将地址持久化存储到本地
     saveAddressToStorage() {
       uni.setStorageSync("address", JSON.stringify(state.address));
+    },
+    //更新用户基本信息
+    updataUserInfo(state, userinfo) {
+      state.userinfo = userinfo;
+      this.commit("m_user/saveUserInfoToStorage");
+    },
+    //将用户信息持久化存储到本地
+    saveUserInfoToStorage(state) {
+      uni.setStorageSync("userinfo", JSON.stringify(state.userinfo));
+    },
+    //更新token字符串
+    updataToken(state, token) {
+      state.token = token;
+      this.commit("m_user/saveTokenStorage");
+    },
+    //将token字符串保存到本地存储中
+    saveTokenStorage(state) {
+      uni.setStorageSync("token", state.token);
     },
   },
   // 数据包装器
